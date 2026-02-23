5daa25f903fdb71c19482e41d6a8fc47d4417ac2
zii002 <zeynepakkurt@gmail.com>
2026-02-23 18:30:58 +0300

    Add database and BicycleTrips table schema

--- 01_create_table.sql
+++ 01_create_table.sql
@@ -1,1 +1,24 @@
+-- Creates the database and the BicycleTrips table schema.
 
+CREATE DATABASE LondonBicycleAnalysis;
+GO
+
+USE LondonBicycleAnalysis;
+GO
+
+CREATE TABLE dbo.BicycleTrips (
+    rental_id                      BIGINT       NULL,
+    duration                       INT          NULL,
+    duration_ms                    BIGINT       NULL,
+    bike_id                        INT          NULL,
+    bike_model                     NVARCHAR(100) NULL,
+    end_date                       NVARCHAR(50)  NULL,   -- text with 'UTC'
+    end_station_id                 INT          NULL,
+    end_station_name               NVARCHAR(200) NULL,
+    start_date                     NVARCHAR(50)  NULL,   -- text with 'UTC'
+    start_station_id               INT          NULL,
+    start_station_name             NVARCHAR(200) NULL,
+    end_station_logical_terminal   INT          NULL,
+    start_station_logical_terminal INT          NULL,
+    end_station_priority_id        INT          NULL
+);
